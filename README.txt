b1ff857428703148bb9bc173e6d417a07df55696
unknown <dariusz.steblik@gmail.com>
2021-02-25 22:47:33 +0100

    modify api test and make readme

--- README.txt
+++ README.txt
@@ -1,17 +1,40 @@
-Do poprawnego uruchomienia projektu należy wykonać poniższe kroki:
-1. Instalacja JDK (rekomendowana wersja 15.0.2)
-2. Instalacja Maven (rekomendowana wersja 3.6.3)
-3. Utworzenie zmiennych środowiskowych: JAVA_HOME, MAVEN_HOME (odnoszących się kolejno do lokalizacji JDK oraz Maven) oraz dodanie lokalizacji MAVEN_HOME do zmiennej Path
-4. Instalacja przeglądarki Chrome (wymagana wersja 88.0.4324.150). W przypadku innej wersji przeglądarki wymagana jest podmiana pliku chromedriver.exe w głównym katalogu programu na wersję zgodną z przeglądarką.
-Po wykonaniu powyższych kroków, projekt można uruchomić z konsoli, w której należy przejść do głównego katalogu programu, a następnie użyć polecenia: "mvn clean install" (w razie problemów alternatywnie można użyć: "mvn clean install -Dtestng.dtd.http=true")
-5. W głównym katalogu projektu, w pliku testng.xml zawarta jest lista wszystkich testów. Aby uruchomić konkretny test case, należy go odkomentować. (do poprawnego działania projektu, powinien być odkomentowany maksymalnie jeden test case, domyślnie uruchomi się test: "UI automation TC 1").
+Instrukcje instalacji oraz uruchomienia:
 
-Linux: 
-1. instalacja git
-2. instalacja jdk
-3. instalacja maven
+WINDOWS: 
+1. Instalacja GIT
+2. Instalacja JDK (rekomendowana wersja 15.0.2)
+3. Instalacja MAVEN (rekomendowana wersja 3.6.3)
+4. Dodać zmienne systemowe MAVEN_HOME, M2_HOME, JAVA_HOME oraz zaktualizować PATH o MAVEN_HOME, JAVA_HOME
+5. Instalacja przeglądarki Chrome (wymagana wersja 88)
+6. Pobrać projekt z repozytorium
+7. Testy można uruchomić z konsoli (konsola uruchomiona z poziomu głównego katalogu projektu) za pomocą komend:
+mvn clean install -Dsurefire.suiteXmlFiles=testngAPI.xml - testy API
+mvn clean install -Dsurefire.suiteXmlFiles=testngUI.xml - testy UI
 
-Uruchamianie testów komendy:
+
+LINUX:
+1. Instalacja GIT
+2. Instalacja JDK
+3. Instalacja MAVEN
+4. Dodać zmienne systemowe MAVEN_HOME, M2_HOME, JAVA_HOME oraz zaktualizować PATH o M2_HOME
+5. Instalacja przeglądarki Chrome (wymagana wersja 88)
+6. Pobrać projekt z repozytorium
+7. Testy można uruchomić z terminala (terminal uruchomiony z poziomu głównego katalogu projektu) za pomocą komend:
+mvn clean install -Dsurefire.suiteXmlFiles=testngAPI.xml - testy API
+mvn clean install -Dsurefire.suiteXmlFiles=testngUI.xml - testy UI
+Pomocne linki:
+https://linuxize.com/post/how-to-install-apache-maven-on-ubuntu-20-04/
+https://linuxize.com/post/how-to-install-git-on-ubuntu-18-04/
+
+
+MAC OS:
+1. Instalacja GIT
+2. Instalacja JDK
+3. Instalacja MAVEN
+4. Dodać zmienne systemowe MAVEN_HOME, M2_HOME, JAVA_HOME oraz zaktualizować PATH o M2_HOME
+5. Instalacja przeglądarki Chrome (wersja 88)
+6. Pobrać projekt z repozytorium
+7. Testy można uruchomić z konsoli (konsola uruchomionq z poziomu głównego katalogu projektu) za pomocą komend:
 mvn clean install -Dsurefire.suiteXmlFiles=testngAPI.xml - testy API
 mvn clean install -Dsurefire.suiteXmlFiles=testngUI.xml - testy UI
 
--- src/test/java/ApiAutomationTest1.java
+++ src/test/java/ApiAutomationTest1.java
@@ -19,6 +19,81 @@ public class ApiAutomationTest1 {
     @Test
     public void apiAutomationTest1() {
 
+        //pobranie za pomoca metod rest assured regionow wraz ze szczegolami
+        List<Regions> regions =
+                given().
+                    log().all().
+                    when().
+                    get("https://api.carbonintensity.org.uk/regional/").
+                    then().
+                    log().all().
+                    statusCode(200)
+                    .extract().as(DataList.class) //DataList wiaze sie z klasami pomocniczymi z paczki: apiObjects
+                    .getData().get(0).getRegions();
+
+
+        //SCENARIUSZE TESTOWE:
+
+        System.out.println("------------------------------");
+        System.out.println("SCENARIO 1:");
+
+        //pobranie oraz wyswietlenie listy shortnames
+        List<Long> regionIds = regions.stream().map(Regions::getRegionid).collect(Collectors.toList());
+        System.out.println(regionIds);
+
+        //pobranie oraz wyswietlenie listy forecastValues
+        List<Integer> forecastValues = regions.stream().map(Regions::getIntensity).map(Intensity::getForecast).collect(Collectors.toList());
+        System.out.println(forecastValues);
+
+        //sortowanie listy od najwiekszej wartosci do najmniejszej oraz wyswietlenie
+        Collections.sort(forecastValues, Collections.reverseOrder());
+        System.out.println(forecastValues);
+
+        //pobranie oraz wyswietlenie listy shortnames
+        List<String> shortNames = regions.stream().map(Regions::getShortname).collect(Collectors.toList());
+//        List<String> shortNames2 = forecastValues.stream().map()
+        System.out.println("Shortnames: " + shortNames.get(2) + ", forecast: " + forecastValues.get(2));
+
+
+        System.out.println("------------------------------");
+        System.out.println("SCENARIO 2:");
+
+        //pobranie oraz wyswietlenie zagniezdzonej listy fuellist
+        List<List<GenerationMix>> fuelList = regions.stream().map(Regions::getGenerationmix).collect(Collectors.toList());
+
+        //oczekiwana wartosc
+        Double expectedValue = 100.0;
+
+        //wyswietlanie sumy pola perc dla kazdego regionu wraz z asercja wartosci
+        for (int i = 0; i<regionIds.size(); i++) {
+            try {
+                Double sum = fuelList.get(i).stream().mapToDouble(GenerationMix::getPerc).sum();
+                Long regionId = regionIds.get(i);
+                Assert.assertEquals(sum, expectedValue);
+                System.out.println("Region ID "  + regionId + ": Sum: " + sum + ", Expected sum: " + expectedValue + " - PASSED");
+            }catch (AssertionError ar) {
+                Double sum = fuelList.get(i).stream().mapToDouble(GenerationMix::getPerc).sum();
+                Long regionId = regionIds.get(i);
+                System.out.println("Region ID "  + regionId + ": Sum: " + sum + ", Expected sum: " + expectedValue + " - FAILED");
+            }
+        }
+
+
+        //System.out.println(fuelList);
+
+
+        //Scenario 3: ---------------------
+//
+//        List<Double> fuelBiomass = fuelList.stream().flatMap(Collection::stream).filter(a -> Objects.equals(a.getFuel(), "biomass"))
+//                .map(a -> a.getPerc())
+//                .collect(Collectors.toList());
+//
+//        System.out.println(fuelBiomass);
+//
+
+//
+// -----------------------------------------------------
+
 //    List<Integer> regionIds =
 //            given().
 //                log().all().
@@ -32,68 +107,8 @@ public class ApiAutomationTest1 {
 //
 //    System.out.println(regionIds);
 
-        List<Regions> regions =
-                given().
-                        log().all().
-                        when().
-                        get("https://api.carbonintensity.org.uk/regional/").
-                        then().
-                        log().all().
-                        statusCode(200)
-                        .extract().as(DataList.class)
-                        .getData().get(0).getRegions();
-
-//        System.out.println(regions);
-
-        List<Long> regionIds = regions.stream().map(Regions::getRegionid).collect(Collectors.toList());
-        System.out.println(regionIds);
-
-        //sortowanie od najwyzszej forecastValues do najnizszej
-        List<Integer> forecastValues = regions.stream().map(Regions::getIntensity).map(Intensity::getForecast).collect(Collectors.toList());
-        System.out.println(forecastValues);
-        Collections.sort(forecastValues, Collections.reverseOrder());
-        System.out.println(forecastValues);
-
-        //
-        List<String> shortNames = regions.stream().map(Regions::getShortname).collect(Collectors.toList());
-        System.out.println(shortNames);
-
-//
-        //sumowanie ------------------
-
-        List<List<GenerationMix>> fuelList = regions.stream().map(Regions::getGenerationmix).collect(Collectors.toList());
-        Double expectedValue = 100.0;
-        for (int i = 0; i<18; i++) {
-            Double sum = fuelList.get(i).stream().mapToDouble(GenerationMix::getPerc).sum();
-            System.out.println(sum);
-            Assert.assertEquals(sum, expectedValue);
-        }
-
-
-        //System.out.println(fuelList);
-
-
-        //trzecie ---------------------
-//
-//        List<Double> fuelBiomass = fuelList.stream().flatMap(Collection::stream).filter(a -> Objects.equals(a.getFuel(), "biomass"))
-//                .map(a -> a.getPerc())
-//                .collect(Collectors.toList());
-//
-//        System.out.println(fuelBiomass);
-//
-
-//
-//
-
-
-
-
 
 //    System.out.println(regionIds);
-
-
-
-
 
 
 //        String data = given().
